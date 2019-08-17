add2ebcd9bdbc8073c9ac763834f722cbd7acc2c
caixw <xnotepad@gmail.com>
2019-08-17 23:39:47 +0800

    采用 gulp

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+
+'use strict';
+
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const del = require('del');
+const path = require('path');
+const sourcemaps = require('gulp-sourcemaps');
+const vsce = require('vsce');
+
+const extension = ts.createProject('tsconfig.json');
+
+ // 编译当前的扩展
+async function compileExtension() {
+    let s = extension.src()
+
+    if (extension.options.sourceMap) {
+        s = s.pipe(sourcemaps.init())
+            .pipe(extension())
+            .pipe(await sourcemaps.write('./'))
+    }
+
+    s.pipe(gulp.dest(extension.options.outDir));
+}
+
+function clean() {
+    return del(extension.options.outDir);
+}
+
+function watch() {
+    gulp.watch(extension.options.rootDir);
+}
+
+function createVSIX() {
+    return vsce.createVSIX();
+}
+
+// 发布当前的扩展
+function publish() {
+    return vsce.publish();
+}
+
+gulp.task('compile', compileExtension);
+
+gulp.task('clean', clean);
+
+gulp.task('watch', watch);
+
+gulp.task('publish', gulp.series(createVSIX, publish));
+
+gulp.task('package', gulp.series(compileExtension, createVSIX));
