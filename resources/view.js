e7698b8192f383eb2147f4202b4812b585c49283
caixw <xnotepad@gmail.com>
2019-08-22 22:27:55 +0800

    添加表格的排序功能

--- resources/view.js
+++ resources/view.js
@@ -7,6 +7,8 @@ window.addEventListener('load', (e) => {
     vscode.postMessage({
         type: 'refresh',
     });
+
+    initSortTable();
 });
 
 window.addEventListener('message', (e) => {
@@ -52,4 +54,61 @@ function appendFileTypeToTr(tr, type) {
     tr.appendChild(document.createElement('td')).append(type.avg);
     tr.appendChild(document.createElement('td')).append(type.max);
     tr.appendChild(document.createElement('td')).append(type.min);
+}
+
+function initSortTable() {
+    const tables = document.querySelectorAll('table');
+
+    tables.forEach((table) => {
+        const ths = table.querySelectorAll('thead>tr>th');
+        ths.forEach((th, index) => {
+            th.setAttribute('data-index', index);
+            th.addEventListener('click', (e) => {
+                const th = e.target;
+                const asc = !(/true/.test(th.getAttribute('data-asc')));
+                const index = parseInt(th.getAttribute('data-index'));
+                const table = th.parentNode.parentNode.parentNode;
+                sortTable(table, index, asc);
+                th.setAttribute('data-asc', asc);
+
+                // 去掉其它元素的 asc 属性
+                th.parentNode.querySelectorAll('th').forEach((val, i)=>{
+                    if (index !== i) {
+                        val.setAttribute('data-asc', 'none');
+                    }
+                })
+            }) // end onclick
+        })
+    }) // end tables.forEach
+}
+
+/**
+ * 对表进行排序，仅处理 tbody 中的内容
+ *
+ * @param {HTMLTableElement} table 需要排序的表元素
+ * @param {number} colIndex 按第几列进行排序
+ * @param {boolean} asc 是否倒序
+ */
+function sortTable(table, colIndex, asc) {
+    let rows = [];
+
+    table.querySelectorAll('tbody>tr').forEach((row) => {
+        rows.push(row);
+    })
+
+
+    rows = rows.sort((row1, row2) => {
+        v1 = row1.cells.item(colIndex).innerHTML.toLowerCase();
+        v2 = row2.cells.item(colIndex).innerHTML.toLowerCase();
+
+        if (asc) {
+            return v1 - v2;
+        }
+        return v2 - v1;
+    })
+
+    const p = rows[0].parentNode;
+    for (const row of rows) {
+        p.appendChild(row);
+    }
 }
