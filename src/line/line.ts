36a981a32dbd9b755b64d976377d7832d6c0aefb
caixw <xnotepad@gmail.com>
2019-08-23 21:49:02 +0800

    添加对空行的统计

--- src/line/line.ts
+++ src/line/line.ts
@@ -8,22 +8,41 @@ const fs = filesystem.promises;
 /**
  * 统计该文件中的行数信息
  *
- * @param path 文件地址
+ * @param p 文件地址
  */
-export async function count(file: string): Promise<Lines> {
-    let name = path.extname(file);
+export async function count(p: string): Promise<Lines> {
+    let name = path.extname(p);
     if (name === '') {
-        name = path.basename(file);
+        name = path.basename(p);
     }
 
-    const content = (await fs.readFile(file, { encoding: 'utf8' }));
-    return {
+    const content = (await fs.readFile(p, { encoding: 'utf8' }));
+    return countContent(name, content);
+}
+
+function countContent(name: string, content: string): Lines {
+    const lines = content.split('\n');
+
+    const ret = {
         name,
-        lines: content.split('\n').length,
+        lines: lines.length,
+        blanks: 0,
     };
+
+    for (let line of lines) {
+        line = line.trim();
+        if (line.length === 0) {
+            ret.blanks++;
+        }
+
+        // TODO
+    }
+
+    return ret;
 }
 
 export interface Lines {
     name: string; // 扩展名或是文件名
     lines: number; // 总行数
+    blanks: number; // 空行的行数
 }
