929e42a3598ee876f1af92333a9198d2ad34f866
caixw <xnotepad@gmail.com>
2019-08-28 21:06:50 +0800

    npm 测试可以指定版本
    可以通过 npm run test insiders 指定只测试 insiders 版本的内容，
    目前支持以下三种版本：
    - insiders 表示 vscode insiders 版本；
    - stable 最新的稳定版；
    - minimum 表示最小的支持版本，该版本号由 pakcage.json/engines.vscode 决定；

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -14,41 +14,92 @@ const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
 // 采用 require 可以避免 package.json 文件不在
 // tsconfig.compilerOptions.rootDir 中的编译错误；
-const pkg = require(path.resolve(rootDir,'package.json'));
+const pkg = require(path.resolve(rootDir, 'package.json'));
+
+/**
+ * 保存从参数中获取的值信息
+ */
+class Options {
+    stable: boolean;
+    minimum: boolean;
+    insiders: boolean;
+
+    constructor(val: boolean) {
+        this.stable = val;
+        this.minimum = val;
+        this.insiders = val;
+    }
+}
 
 async function main() {
+    const o = parseArgs();
+
     try {
         // 测试当前版本
-        await runTests({
-            extensionDevelopmentPath:rootDir,
-            extensionTestsPath,
-            version: 'stable',
-            launchArgs: [rootDir],
-        });
+        if (o.stable) {
+            await runTests({
+                extensionDevelopmentPath: rootDir,
+                extensionTestsPath,
+                version: 'stable',
+                launchArgs: [rootDir],
+            });
+        }
 
         // 测试最低需求的版本
-        let minimum = pkg.engines.vscode.slice(1);
-        if (isNaN(minimum.charAt(0))) {
-            minimum = minimum.slice(1);
+        if (o.minimum) {
+            let minimum = pkg.engines.vscode.slice(1);
+            if (isNaN(minimum.charAt(0))) {
+                minimum = minimum.slice(1);
+            }
+            await runTests({
+                extensionDevelopmentPath: rootDir,
+                extensionTestsPath,
+                version: minimum,
+                launchArgs: [rootDir],
+            });
         }
-        await runTests({
-            extensionDevelopmentPath:rootDir,
-            extensionTestsPath,
-            version: minimum,
-            launchArgs: [rootDir],
-        });
 
         // 测试 insiders 版本
-        await runTests({
-            extensionDevelopmentPath:rootDir,
-            extensionTestsPath,
-            version: 'insiders',
-            launchArgs: [rootDir],
-        });
+        if (o.insiders) {
+            await runTests({
+                extensionDevelopmentPath: rootDir,
+                extensionTestsPath,
+                version: 'insiders',
+                launchArgs: [rootDir],
+            });
+        }
     } catch (err) {
         throw err;
     }
 }
+
+function parseArgs(): Options {
+    // 当前是从 npm script 中过来的，所以前两个参数需要过滤
+    const args = process.argv.slice(2);
+    if (args.length === 0) {
+        return new Options(true);
+    }
+
+    let o = new Options(false);
+    const keys = args[0].split(',');
+    for (const key of keys) {
+        switch (key.toLowerCase()) {
+            case 'stable':
+                o.stable = true;
+                break;
+            case 'minimum':
+                o.minimum = true;
+                break;
+            case 'insiders':
+                o.insiders = true;
+                break;
+            default:
+                throw new Error(`无效的参数值${key}`);
+        }
+    }
+    return o;
+}
+
 
 main().catch((reason) => {
     console.error(reason);
